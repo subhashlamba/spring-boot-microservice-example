bcb9b744dd5677aea29fe907689cf489ae0c0dd7
Subhash Lamba <subhashlammba@gmail.com>
2021-05-08 12:22:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-mvn clean install -f ./spring-boot-cloud-eureka-server/pom.xml
-mvn clean install -f ./spring-boot-cloud-eureka-account-service/pom.xml
-mvn clean install -f ./spring-boot-cloud-zuul-routing/pom.xml
+mvn clean install -f ./spring-boot-cloud-eureka-server\pom.xml
+mvn clean install -f ./spring-boot-cloud-zuul-routing\pom.xml
+mvn clean install -f ./spring-boot-cloud-eureka-user-service\pom.xml
+mvn clean install -f ./spring-boot-cloud-eureka-order-service\pom.xml
+mvn clean install -f ./spring-boot-cloud-authentication-service\pom.xml
 
-java -jar spring-boot-cloud-eureka-server/target/eureka-server.jar &
-java -jar spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8181 &
-java -jar spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8182 &
-java -jar spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8183 &
-java -jar spring-boot-cloud-zuul-routing/target/zuul-router.jar --server.port=8080 &
-
+java -jar spring-boot-cloud-eureka-server/target/eureka-server.jar  &
+java -jar spring-boot-cloud-zuul-routing/target/zuul-api-gateway.jar --server.port=8080  &
+java -jar spring-boot-cloud-eureka-user-service/target/user-service.jar --server.port=8181 &
+java -jar spring-boot-cloud-eureka-order-service/target/order-service.jar --server.port=8282 &
+java -jar spring-boot-cloud-authentication-service/target/authentication-service.jar --server.port=8383 &

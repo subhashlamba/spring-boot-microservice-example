3c41faf2a6c3dd115e2278003e309abda9be2cc5
Subhash Lamba <subhashlammba@gmail.com>
2021-05-08 12:20:52 +0530

    Update microservice.cmd

--- microservice.cmd
+++ microservice.cmd
@@ -1,12 +1,11 @@
+mvn clean install -f .\spring-boot-cloud-eureka-server\pom.xml
+mvn clean install -f .\spring-boot-cloud-zuul-routing\pom.xml
+mvn clean install -f .\spring-boot-cloud-eureka-user-service\pom.xml
+mvn clean install -f .\spring-boot-cloud-eureka-order-service\pom.xml
+mvn clean install -f .\spring-boot-cloud-authentication-service\pom.xml
 
-mvn clean install -f ./spring-boot-cloud-eureka-server/pom.xml
-mvn clean install -f ./spring-boot-cloud-eureka-account-service/pom.xml
-mvn clean install -f ./spring-boot-cloud-zuul-routing/pom.xml
-
-START "" java -jar spring-boot-cloud-eureka-server/target/eureka-server.jar 
-START "" java -jar spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8181
-START "" java -jar spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8182
-START "" java -jar spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8183
-START "" java -jar spring-boot-cloud-zuul-routing/target/zuul-router.jar --server.port=8080
-
-
+START "Server" java -jar spring-boot-cloud-eureka-server/target/eureka-server.jar 
+START "API Gateway" java -jar spring-boot-cloud-zuul-routing/target/zuul-api-gateway.jar --server.port=8080 
+START "User Service" java -jar spring-boot-cloud-eureka-user-service/target/user-service.jar --server.port=8181
+START "Order Service" java -jar spring-boot-cloud-eureka-order-service/target/order-service.jar --server.port=8282
+START "Authentication Service" java -jar spring-boot-cloud-authentication-service/target/authentication-service.jar --server.port=8383
